8885a3a4bdeeeefb24b513015573938ab0993a0f
Paul LeTourneau <pletourneau62@gmail.com>
2023-11-15 09:10:24 -0800

    initial commit

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,29 @@
+// import React from "react";
+// import PropTypes from "prop-types";
+// import { v4 } from "uuid";
+// import ReusableForm from "./ReusableForm";
+
+// function NewTicketForm(props) {
+//   return (
+//     <React.Fragment>
+//       <ReusableForm
+//         formSubmissionHandler={handleNewTicketFormSubmission}
+//         buttonText="Help!"
+//       />
+//     </React.Fragment>
+//   );
+//   function handleNewTicketFormSubmission(event) {
+//     props.onNewTicketCreation({
+//       names: event.target.names.value,
+//       location: event.target.location.value,
+//       issue: event.target.issue.value,
+//       id: v4(),
+//     });
+//   }
+// }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func,
+};
+
+export default NewTicketForm;
